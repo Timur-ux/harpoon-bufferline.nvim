9ede5bbd31e2aa5df2fcb19f824548341c7a1819
Tias <timur.buchkin@mail.ru>
2026-05-16 18:31:22 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+ {
+  description = "Harpoon + bufferline vimPlugin flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.vimUtils.buildVimPlugin {
+					 name = "harpoon-bufferline";
+					 src = ./.;
+				 };
+      }
+    );
+}
